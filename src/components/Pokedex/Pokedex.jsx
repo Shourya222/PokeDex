1638edc904a2438c896d88b6397737e530086f84
shourya <shouryaverma222@gmail.com>
2025-07-28 18:51:21 +0530

    Added Debounce

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import PokemonList from '../PokemonList/PokemonList';
 import Search from '../Search/Search';
 import './Pokedex.css';
+import PokemonDetails from '../PokemonDetails/PokemonDetails';
 
 function Pokedex(){
+
+  const [searchTerm, setSeacrchTerm] = useState('');
     return(
         <div className='pokedex-wrapper'>
           <h1>Pokedex</h1>
-          <Search/>
-          <PokemonList/>
+          <Search updateSearchTerm={setSeacrchTerm}/>
+          {searchTerm ? <PokemonDetails pokemonName={searchTerm}/> : <PokemonList/>}
         </div>
     )
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,16 @@
+import useDebounce from '../../hooks/useDebounce';
 import './Search.css';
 
-function Search(){
+function Search({ updateSearchTerm }){
+    const debounceUpdateSearch = useDebounce((e) => updateSearchTerm(e.target.value));
     return(
         <input 
         id='search-pokemon'
         type="text" 
-        placeholder='which pokemon you are looking for ?'/>
+        placeholder='which pokemon you are looking for ?'
+        onChange = {debounceUpdateSearch}
+        />
+        
     )
 }
 
